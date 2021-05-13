39f63fe21491962329b1f7b38ddf1fdfde5cbc18
thisitha <thisithakavinda@gmail.com>
2021-05-14 02:42:36 +0530

    addming all

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ from main import chatWithBot
 app = Flask(__name__)
 
 
+@app.route('/chat', methods=['GET', 'POST'])
+def chatBot():
+    chatInput = request.form['chatInput']
+    return jsonify(chatBotReply=chatWithBot(chatInput))
 
 
-
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
 
 
 if __name__ == '__main__':
